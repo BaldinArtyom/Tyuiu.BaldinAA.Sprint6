c3918f32bb8ad1369b25e269f02ebb1cc91948b8
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-12-13 00:16:51 +0500

    task1

--- Tyuiu.BaldinAA.Sprint6.Task1.V19.Lib/DataService.cs
+++ Tyuiu.BaldinAA.Sprint6.Task1.V19.Lib/DataService.cs
@@ -5,16 +5,16 @@ namespace Tyuiu.BaldinAA.Sprint6.Task1.V19.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            double[] res = new int[-5, 5];
+            double[] res = new double[stopValue - startValue + 1];
             for (int i = 0; i < res.Length; i++)
             {
-                if (i == -2)
+                if (i-stopValue == -2)
                 {
                     res[i] = 0;
                 }
                 else
                 {
-                    res[i] = Math.Round(Math.Cos(i) + (Math.Cos(i)) / (i + 2) - 3 * i, 3);
+                    res[i] = Math.Round(Math.Cos(i-stopValue) + (Math.Cos(i-stopValue)) / (i-stopValue + 2) - 3 * (i-stopValue), 3);
                 }
             }
             return res; 
