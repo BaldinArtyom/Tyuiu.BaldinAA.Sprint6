57c335b8f9a1dfee22ac5a50d23557b96a462e1e
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-12-12 20:36:08 +0500

    Task4

---
+++ Tyuiu.BaldinAA.Sprint6.Task4.V11/FormMain.cs
@@ -0,0 +1,72 @@
+﻿using Tyuiu.BaldinAA.Sprint6.Task4.V11.Lib;
+namespace Tyuiu.BaldinAA.Sprint6.Task4.V11
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        DataService ds = new DataService();
+
+        private void buttonData_TAV_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 4 выполнил студент группы ИБКСб-24-1 Балдин Артём Александрович", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void buttonResult_TAV_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int startStep = Convert.ToInt32(textBoxStartStep_TAV.Text);
+                int stopStep = Convert.ToInt32(textBoxStopStep_TAV.Text);
+                int len = ds.GetMassFunction(startStep, stopStep).Length;
+                double[] valueArray;
+                valueArray = new double[len];
+                valueArray = ds.GetMassFunction(startStep, stopStep);
+                this.chartFunction.Titles.Add("f(x)");
+                this.chartFunction.ChartAreas[0].AxisX.Title = "Введите X";
+                this.chartFunction.ChartAreas[0].AxisY.Title = "Введите Y";
+                textBoxResult.Text = "";
+                chartFunction.Series[0].Points.Clear();
+                for (int i = 0; i <= len - 1; i++)
+                {
+                    this.chartFunction.Series[0].Points.AddXY(startStep, valueArray[i]);
+                    textBoxResult.AppendText(valueArray[i] + Environment.NewLine);
+                    startStep++;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ââåäåíû íåâåðíûå äàííûå", "Îøèáêà", MessageBoxButtons.OK);
+            }
+        }
+
+        private void textBoxStartStep_TAV_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSave_TAV_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask4V24.txt";
+                File.WriteAllText(path, textBoxResult.Text);
+                DialogResult dialogResult = MessageBox.Show("Таск " + path + " Ошибка!\nВерно?", "Сообщение", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    System.Diagnostics.Process txt = new System.Diagnostics.Process();
+                    txt.StartInfo.FileName = "notepad.exe";
+                    txt.StartInfo.Arguments = path;
+                    txt.Start();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
+
